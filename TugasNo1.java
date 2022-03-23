af071f95de528a1f286520cfc2007ac2e606a9ca
nuralampakubumi <101490279+nuralampakubumi@users.noreply.github.com>
2022-03-23 14:25:19 +0800

    Add files via upload

---
+++ TugasNo1.java
@@ -0,0 +1,9 @@
+//13020200039_Nuralam Paku Bumi_jumat/18 maret 20.00
+
+public class TugasNo1 {
+	public static void main(String[] args){	
+		int nomor;	
+		nomor = 5;
+		System.out.print(nomor*2);
+	}
+}
